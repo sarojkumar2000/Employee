0f8f923da106811606295c7eac503c6652948d25
sarojkumar2000 <60341032+sarojkumar2000@users.noreply.github.com>
2020-09-01 13:36:40 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,14 @@
+
+function validate(){
+    var name=document.getElementById("name").value
+    var pwd=document.getElementById("pwd").value;
+    if(name=="saroj@gmail.com" && pwd=="saroj@13"){
+        return true
+    }
+    else{
+        return false;
+    }
+    
+   
+    
+}
